aea862174488b2f2b7f9cc71cb62e5ccdc004dfa
Michael Ferguson <mike@shyftsolutions.io>
2022-05-07 21:22:21 +0000

    Updated streamlit

--- python/src/main.py
+++ python/src/main.py
@@ -1,11 +1,12 @@
 """The main entry for the streamlit app"""
 
+import pandas as pd
 import streamlit as st
 
 
 def handle_log_click():
     # TODO: replace with a service call
-    print("Clicked!!!")
+    print("log")
 
 
 def render():
@@ -14,8 +15,10 @@ def render():
         # Throw some buttons on here
         """
     )
-
+    data = pd.DataFrame([[1, 2], [3, 4]], columns=["test1", "test2"])
     st.button("Log", on_click=handle_log_click)
+    st.write(data)
+    st.bar_chart(data)
 
 
 render()
